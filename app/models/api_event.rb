a44d990af43916feeac91ec20ab0a7591d0aa7e0
kohhata <kohhata@opendoor.co.jp>
2017-10-26 20:28:19 +0900

    API結果モデルのテンプレ作成

---
+++ app/models/api_event.rb
@@ -0,0 +1,20 @@
+class ApiEvent
+
+  attr_accessor :eventid, :images, :title, :description, :prefecure, :category
+
+  def find_recently()
+    list = []
+
+  end
+
+  def find_popularity()
+
+
+  end
+
+  def find_near()
+
+
+  end
+
+end
