8619a45bed90e32d2b09d919a25ff505c00aa99b
kohhata <kohhata@opendoor.co.jp>
2017-10-27 19:13:03 +0900

    modelのベース作成

---
+++ spec/factories/api_events.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :api_event do
+    
+  end
+end
---
+++ spec/models/api_event_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ApiEvent, type: :model do
+
+  # API接続テスト
+   describe '#model validate' do
+
+     describe '#model' do
+       it "API接続" do
+        #  events = ApiEvent.find_all()
+
+       end
+     end
+  end
+end
