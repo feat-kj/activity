d2680731a6a6e38bc6f02edc47b1013e39b58130
kohhata <kohhata@opendoor.co.jp>
2018-01-16 19:07:00 +0900

    aaa

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,9 +1,9 @@
 class Spot < ApplicationRecord
 
   belongs_to :prefecture
-  has_one  :spot_detail, :dependent => :destroy
+  has_one  :spot_detail, dependent: :destroy, class_name: SpotDetail
   has_many :parkings
-  has_many :spot_terms, :dependent => :destroy
+  has_many :spot_terms, dependent: :destroy
   has_many :spot_genres
   has_many :genres, through: :spot_genres
 
--- lib/tasks/api_import.rb
+++ lib/tasks/api_import.rb
@@ -2,18 +2,34 @@ class Tasks::ApiImport
 
   def self.execute
       Rails.logger.debug("api import")
-      responses = ApiEvent.find_popular
-      p "Hello world"
+      responses = ApiEvent.find_recently
+      prefectures = {}
+      Prefecture.all.each{|item| prefectures[item.name] = item }
+      # p prefectures
+
       responses.each {|event|
+
         # p event
         spot = Spot.new
+
+        # 管理情報
+        spot.ref_city_code  = event.dig("mng", "lgcode")
+        spot.ref_name       = event.dig("mng", "data_source")
+        spot.ref_id         = event.dig("mng", "refbase")
+        spot.ref_sub_id     = event.dig("mng", "refsub")
+        spot.ref_updated_at = event.dig("mng", "status", "udate")
+
+        # 基本情報
         spot.name        = event.dig("name", "name1", "written")
         spot.name_spoken = event.dig("name", "name1", "spoken")
         spot.body        = event["descs"].first unless event["descs"].nil?
         spot.longitude   = event.dig("place", "coordinates", "longitude")
         spot.latitude    = event.dig("place", "coordinates", "longitude")
         spot.zip         = event.dig("place", "postal_code")
-        # t.belongs_to :prefecture, nil:false
+        spot.active_term = event.dig("visit", "service", "open")
+
+        # 連絡先
+        spot.prefecture = prefectures[event.dig("place", "pref", "written")]
         spot.city     = event["place"]["city"]["written"]
         spot.address1 = event.dig("place", "street", "written")
         spot.address2 = event.dig("place", "building", "written")
@@ -22,32 +38,71 @@ class Tasks::ApiImport
         spot.email    = event.dig("place", "email")
         spot.url      = event.dig("url")
 
-        unless event.dig("views").nil? {
+        # 画像情報
+        unless event.dig("views").nil?
           event_info = event.dig("views").first
           spot.image_url           = event_info.dig("fid")
           spot.image_copyright     = event_info.dig("copyright")
           spot.image_name          = event_info.dig("name", "written")
           spot.image_spoken        = event_info.dig("name", "spoken")
           spot.image_shooting_date = event_info.dig("when")
-        }
+        end
 
+        # スポット詳細
+        detail = SpotDetail.new
+        detail.sub_name        = event.dig("name", "name2", "written")
+        detail.sub_name_spoken = event.dig("name", "name2", "spoken")
+        detail.guide_reserve   = event.dig("guide", "reservation")
+        detail.guide_charge    = event.dig("guide", "charge")
+        detail.guide_tel       = event.dig("guide", "phone")
+        detail.guide_fax       = event.dig("guide", "fax")
+        detail.guide_email     = event.dig("guide", "email")
+        detail.guide_url       = event.dig("guide", "url")
+        detail.guide_info      = event.dig("guide", "note")
+        detail.wifi            = event.dig("wifi", "established")
+        detail.bus_pickup      = event.dig("visit", "bus", "pickup_place")
+        detail.bus_frequency   = event.dig("visit", "bus", "operation", "frequency")
+        detail.bus_operation_time = event.dig("visit", "bus", "operation", "hour")
+        detail.bus_advance_notice = event.dig("visit", "bus", "advanced", "notice")
+        detail.written_us         = event.dig("foreign", "written", "lang1")
+        detail.written_ch         = event.dig("foreign", "written", "lang3")
+        detail.written_zh         = event.dig("foreign", "written", "lang2")
+        detail.written_gr         = event.dig("foreign", "written", "lang7")
+        detail.written_fr         = event.dig("foreign", "written", "lang6")
+        detail.written_it         = event.dig("foreign", "written", "lang8")
+        detail.written_es         = event.dig("foreign", "written", "lang10")
+        spot.spot_detail = detail
 
-        # spot.active_term
-        # spot.image_url
-        # spot.image_copyright
-        # spot.image_name
-        # spot.image_spoken
-        # spot.image_shooting_date
-        # spot.ref_city_code, nil:false
-        # spot.ref_name, nil:false
-        # spot.ref_id, nil:false
-        # spot.ref_sub_id, nil:false
-        # spot.ref_updated_at, nil:false
-
-        p spot
+        # 開催期間
+        periods = event.dig("visit", "service", "periods")
+        spot_terms = []
+        unless periods.nil?
+          spot_terms = periods.map{|item|
+            spot_term = SpotTerm.new
+            # spot_term.type = item.dig("type")
+            spot_term.season = item.dig("season")
+            spot_term.open_date = item.dig("st_date")
+            spot_term.close_date = item.dig("en_date")
+            spot_term.day_of_week = item.dig("day_of_week")
+            spot_term.hour = item.dig("hours")
+            spot_term.info = item.dig("note")
+          }
+        end
+        spot.spot_terms ||= spot_terms
+        # 登録処理
+        begin
+          Spot.transaction do
+            spot.save!
+            p "commit OK spotid:#{spot.id}"
+          end
+        rescue => e
+          Rails.logger.fatal(e)
+          p e
+          next
+        end
       }
 
-  end
 
+  end
 
 end
